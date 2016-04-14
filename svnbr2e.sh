0c106f8ccd55304ab9c6aa954a73693a4689bcf5
Andreas Henriksson <andreas@fatal.se>
2016-04-14 10:27:29 +0200

    svnbr2e.sh: handle running from existing checkout.
    Skip the prompting for doing the actions while at it, but
    most importantly support running from an existing svn (unstable)
    checkout branch without giving any parameters.

--- svnbr2e.sh
+++ svnbr2e.sh
@@ -1,47 +1,50 @@
 #!/bin/sh
 
+# first check if we're running from a current checkout.
+
+if [ -d .svn ] && [ $# = 0 ]; then
+	URL=$(svn info | awk  '/^URL: /{print $2}')
+	if echo $URL | grep -q '/unstable/' ; then
+		BRANCHU="$URL"
+		BRANCHEXP=$(echo $URL | sed -e 's#/unstable/#/experimental/#')
+		DOSWITCH=1
+	fi
+fi
+
 # ./svnbr2e.sh [desktop|packages] <package>
 
 gnomesuite=desktop
-distribution=unstable
 
-if [ $# != 1 ]
+if [ $# -gt 1 ]
 then
 	if [ "$1" = "packages" ] || [ "$1" = "desktop" ]; then
 		gnomesuite=$1
 		shift
 	fi
-
-	#if [ "$1" = "experimental" ] || [ "$1" = "unstable" ]; then
-	#	distribution=$1
-	#	shift
-	#fi
 fi
 
 if [ $# != 1 ]
 then
-	echo "Usage: $0 [desktop|packages] <package>" >&2
+	if [ -z $BRANCHU ] || [ -z $BRANCHEXP ]; then
+		echo "Usage: $0 [desktop|packages] <package>" >&2
+		exit 1
+	fi
+fi
+
+package=${1:-current}
+
+BRANCHU=${BRANCHU:-"svn+ssh://svn.debian.org/svn/pkg-gnome/$gnomesuite/unstable/$package"}
+BRANCHEXP=${BRANCHEXP:-"svn+ssh://svn.debian.org/svn/pkg-gnome/$gnomesuite/experimental/$package"}
+
+if svn ls "$BRANCHEXP" --depth empty > /dev/null 2>&1 ; then
+	echo "ERROR: experimental branch already exists!" >&2
 	exit 1
 fi
 
-package=$1
+svn cp -m "Branch $package to experimental" \
+	$BRANCHU \
+	$BRANCHEXP
 
-unstableurl="svn+ssh://svn.debian.org/svn/pkg-gnome/$gnomesuite/unstable/$package"
-experimentalurl="svn+ssh://svn.debian.org/svn/pkg-gnome/$gnomesuite/experimental/$package"
-
-if svn ls $experimentalurl > /dev/null 2>&1 ; then
-	echo "Hey! experimental branch already exists!" >&2
-	exit 1
+if [ -n $DOSWITCH ]; then
+	svn switch $BRANCHEXP
 fi
-
-
-echo "About to do:"
-echo "
-svn cp \\
-	$unstableurl \\
-	$experimentalurl
-"
-
-read -p "Press any key to continue...." FOOBAR
-
-svn cp $unstableurl $experimentalurl
